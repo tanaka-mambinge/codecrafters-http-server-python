4f2d61e0ca87ce420ed533515067ea6e10191638
Tanaka Mambinge <83222601+tanaka-mambinge@users.noreply.github.com>
2024-07-23 09:56:50 +0200

    extract path

--- app/main.py
+++ app/main.py
@@ -9,8 +9,24 @@ def main():
     soc, add = server_socket.accept()
 
     # Respond to the client with http 200 OK response
-    res = soc.send(b"HTTP/1.1 200 OK\r\n\r\n")
-    print(f"Response: {res}")
+    # res = soc.send(b"HTTP/1.1 200 OK\r\n\r\n")
+    # print(f"Response: {res}")
+
+    # Extract url path from the request
+    data = soc.recv(1024)
+    path = data.decode().split()[1]
+
+    # Check if the path is registered
+    registered_paths = ["/"]
+    if path not in registered_paths:
+        res = soc.send(b"HTTP/1.1 404 Not Found\r\n\r\n")
+        print(f"Response: {res}")
+    else:
+        res = soc.send(b"HTTP/1.1 200 OK\r\n\r\n")
+        print(f"Response: {res}")
+
+    # Close the socket
+    soc.close()
 
 
 if __name__ == "__main__":
